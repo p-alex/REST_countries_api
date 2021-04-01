415329ab0602be2af332882c64074c8c264ed53f
p-alex <alexey9021@gmail.com>
2021-04-01 18:18:11 +0300

    Added TopBar component

---
+++ src/components/TopBar/TopBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./TopBar.scss";
+const TopBar = () => {
+  return (
+    <header className="topBar">
+      <div className="topBar_container">
+        <h2 className="topBar_logo">Where in the world?</h2>
+      </div>
+    </header>
+  );
+};
+
+export default TopBar;
